0b456f58298075931054755f5a5ee4c23129336b
joctan-tec <joctan@estudiantec.cr>
2025-04-02 23:01:18 -0600

    subiendo archivos principales

---
+++ constantes.py
@@ -0,0 +1,11 @@
+# Parámetros juego
+ANCHO_PANTALLA = 640
+ALTO_PANTALLA  = 480
+VELOCIDAD = 5
+TAMANNO_BLOQUE = 80 # Sirve bien con 20, 40, 80
+
+# Parámetros algoritmo genetico
+CANTIDAD_AGENTES = 30
+CANTIDAD_GENERACIONES = 50
+
+TASA_MUTACION = 0.1
